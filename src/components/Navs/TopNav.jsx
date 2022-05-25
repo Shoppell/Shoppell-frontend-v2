5385be8218b1e9427d6fb9530799966f35fe576c
navid falah <navid.falah7@gmail.com>
2022-05-26 01:54:15 +0430

    navbar edited

--- src/components/Navs/TopNav.jsx
+++ src/components/Navs/TopNav.jsx
@@ -11,28 +11,35 @@ const TopNav = () => {
 
  <div>
 
-<nav class="navbar navbar-expand-lg navbar-light bg-light">
-  <a class="navbar-brand" href="#">Navbar</a>
-  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-    <span class="navbar-toggler-icon"></span>
-  </button>
-  <div class="collapse navbar-collapse" id="navbarNav">
-    <ul class="navbar-nav">
-      <li class="nav-item active">
-        <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link" href="#">Features</a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link" href="#">Pricing</a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link disabled" href="#">Disabled</a>
-      </li>
-    </ul>
-  </div>
-</nav>
+<header id="header" className="fixed-top" style={{backgroundColor:"#D6E0F0"}} >
+        <div className="container d-flex align-items-center justify-content-between">
+          <h1 className="logo" style={{float:"left",marginTop:"1%"}}><a href="index.html">شاپل</a></h1>
+          {/* Uncomment below if you prefer to use an image logo */}
+          {/* <a href="index.html" class="logo"><img src="assets/img/logo.png" alt="" class="img-fluid"></a>*/}
+          <nav id="navbar" className="navbar" style={{margin:"0px"}}>
+            <ul>
+              <li><a className="active" href="index.html" style={{fontFamily:"Yekan"}}>خانه</a></li>
+              <li><a href="our-story.html" style={{fontFamily:"Yekan"}}>درباره ی ما</a></li>
+              <li><a href="events.html" style={{fontFamily:"Yekan"}}>ارتباط با ما</a></li>
+              <li><a href="gallery.html" style={{fontFamily:"Yekan"}}>فروشگاه ها</a></li>
+              <li className="dropdown"><a href="#" style={{fontFamily:"Yekan"}}><span>دسته ها</span> <i className="bi bi-chevron-down" /></a>
+                <ul>
+                  <li><a href="#" style={{fontFamily:"Yekan"}}>لباس</a></li>
+                  <li className="dropdown"><a href="#" style={{fontFamily:"Yekan"}}><span>لوازم برقی</span> <i className="bi bi-chevron-right" /></a>
+                    <ul>
+                      <li><a href="#" style={{fontFamily:"Yekan"}}>موبایل</a></li>
+                      <li><a href="#" style={{fontFamily:"Yekan"}}>تبلت</a></li>
+                      <li><a href="#" style={{fontFamily:"Yekan"}}>لپ تاپ</a></li>
+                    </ul>
+                  </li>
+                </ul>
+              </li>
+              <li><a href="contact.html" style={{fontFamily:"Yekan"}}>همکاری با ما</a></li>
+            </ul>
+            <i className="bi bi-list mobile-nav-toggle" />
+          </nav>{/* .navbar */}
+        </div>
+      </header>{/* End Header */}
      
  </div>
 
